4b8ca36ea072caf4cf80ece6274adf2ec225a4f9
shadowsama <joker8cry@gmail.com>
2025-02-25 18:01:26 +0800

    first_page_Revise

--- static/js/header.js
+++ static/js/header.js
@@ -1,9 +1,10 @@
 function headimg_btn(){
     const headimg_btn = document.querySelector('.headimg_menu').style.display
-    if (headimg_btn === 'block'){
+    if (headimg_btn === 'grid'){
         document.querySelector('.headimg_menu').style.display = 'none'
     } else {
-        document.querySelector('.headimg_menu').style.display = 'block'
+        document.querySelector('.headimg_menu').style.display = 'grid'
     }
 
 }
+
